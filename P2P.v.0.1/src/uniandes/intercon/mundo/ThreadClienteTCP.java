2a54b37425b0fe23e7b4b7a099188de5b1a81338
MysticAlarion <MysticAlarion@c116ea52-a4ca-11de-ac05-951101a8bf3e>
2009-09-21 04:35:52 +0000

    P2P-Netbeans News MVC2

--- P2P.v.0.1/src/uniandes/intercon/mundo/ThreadClienteTCP.java
+++ P2P.v.0.1/src/uniandes/intercon/mundo/ThreadClienteTCP.java
@@ -5,6 +5,7 @@
 package uniandes.intercon.mundo;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -50,34 +51,52 @@ public class ThreadClienteTCP extends Thread {
         System.out.println(">>" + out);
         System.out.println(">>" + in);
         out = new DataOutputStream(cliente.getOutputStream());
-        in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-        System.out.println(">>" + out);
-        System.out.println(">>" + in);
+        //in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+        DataInputStream dis = new DataInputStream(cliente.getInputStream());
+        System.out.println("<<" + out);
+        System.out.println("<<" + in);
         String mensaje = "APLICACION_LIN:" + ap;
         String answer = "";
-        out.writeBytes(mensaje);
-        answer = in.readLine();
+        out.writeUTF(mensaje);
+        answer = dis.readUTF();
+        System.out.println(answer);
+        out.close();
+        dis.close();
+        cliente.close();
         return answer;
     }
 
-    public String darNumeroParametros() throws Exception {
+    public String darNumeroParametros() throws Exception 
+    {
+        cliente = new Socket(host, puerto);
+        out = new DataOutputStream(cliente.getOutputStream());
+        DataInputStream dis = new DataInputStream(cliente.getInputStream());
+
         String mensaje = "APLICACION_NUMPAR:" + ap;
         String answer = "";
         out.writeBytes(mensaje);
-        answer = in.readLine();
+        answer = dis.readUTF();
+        out.close();
+        dis.close();
+        cliente.close();
         return answer;
     }
 
-    public String darRespuesta(String[] parametros) throws Exception {
+    public String darRespuesta(String[] parametros) throws Exception 
+    {
+        cliente = new Socket(host, puerto);
+        out = new DataOutputStream(cliente.getOutputStream());
+        DataInputStream dis = new DataInputStream(cliente.getInputStream());
+
         String mensaje = "APLICACION_PARAMS:" + ap;
         for (int i = 0; i < parametros.length; i++) {
             mensaje += ":" + parametros[i];
         }
         String answer = "";
         out.writeBytes(mensaje);
-        answer = in.readLine();
+        answer = dis.readUTF();
         out.close();
-        in.close();
+        dis.close();
         cliente.close();
         return answer;
     }
